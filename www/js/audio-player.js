97018c99dff56aca7237f6a17a5b962b7d3ff31f
rod <rod@roddickinson.net>
2018-03-24 19:59:02 +0000

    first commit

---
+++ www/js/audio-player.js
@@ -0,0 +1,118 @@
+var audio = {
+	isPlaying: [],
+	objectList: {},
+	currPosition: 0,
+	getDevicePath: function() {
+	    var path;
+	    var platform = device.platform;
+    	platform = platform.toLowerCase();
+	    if (platform == "ios") {
+	        var path = "cdvfile://localhost/bundle/www/";
+	    } else if (platform == "android") {
+	        var path = "file:///android_asset/www/";
+	    };
+	    /*path = window.location.pathname;
+	    var sizefilename = path.length - (path.lastIndexOf("/")+1);
+	    path = path.substr( path, path.length - sizefilename );*/
+	    return path;
+	},
+	createAudioObject: function(filepath) {
+		var url = audio.getDevicePath() + filepath;
+		console.log("\nfileppath used\n" + url);
+	    var mediaObject = new Media(url,
+	            // success callback
+	            function () { 
+	            	console.log("Audio has played - Success"); 
+	            	audio.deleteFromisPlaying(filepath); //deleteFromisPlaying
+	            },
+	            // error callback
+	            function (err) { console.log("Audio object Error: " + JSON.stringify(err) ); }
+	    );
+	    return mediaObject;
+	},
+	addToisPlaying: function(filepath) {
+		//add to isPlaying list
+		audio.isPlaying.push(filepath); 
+	},
+	deleteFromisPlaying: function(index) {
+		//delete from isPlaying list
+		audio.isPlaying.splice(index, 1); //
+	},
+	getObject: function(filepath) {
+		var audioObject;
+		if ( !((audio.getDevicePath() + filepath) in audio.objectList) ) {
+			//object in objectList doesn't exist so create audioOject
+			audioObject = audio.createAudioObject(filepath);
+			audio.objectList[audioObject.src] = audioObject; //add to objectList
+			console.log(JSON.stringify(audio.objectList));
+		} else {
+			//object exists so find it and return it
+			audioObject = audio.objectList[audio.getDevicePath() + filepath]
+		};
+		return audioObject;
+	},
+	isCurrentlyPlaying: function(filepath) {
+		if (audio.isPlaying.indexOf(filepath) != -1) {
+			return true;
+		} else {
+			return false;
+		};	
+	},
+	play: function(filepath) {
+		console.log(audio.isPlaying)
+		console.log(audio.isPlaying.indexOf(filepath));
+
+		var audioObject = audio.getObject(filepath);
+		//if audio is NOT playing
+		if (!(audio.isCurrentlyPlaying(filepath))) {
+			//is not playing 
+			audio.addToisPlaying(filepath); //add to list of playing audio
+			audioObject.play();	//play
+		};	
+	}, 
+	pause: function(filepath) {
+		console.log("pause clicked - audio.isCurrentlyPlaying: " + audio.isCurrentlyPlaying(filepath)); 
+		//if audio is playing
+		if (audio.isCurrentlyPlaying(filepath)) {
+			//is playing so find object and pause
+			var audioObject = audio.objectList[audio.getDevicePath() + filepath]; //search objectList for object
+			console.log(JSON.stringify(audioObject.src) + " is playing and will be paused" + "\n object: \n" + JSON.stringify(audioObject));
+			audioObject.pause();
+			//delete from isPlaying list
+			var isPlayingIndex = audio.isPlaying.indexOf(filepath);
+			audio.deleteFromisPlaying(isPlayingIndex); 
+		};
+	},
+	getDuration: function(filepath) {
+		var audioObject = audio.getObject(filepath);
+		console.log("getDuration object:" + JSON.stringify(audioObject));
+		return audioObject.getDuration();
+	},
+	seekTo: function(filepath, milliseconds) {
+		var audioObject = audio.getObject(filepath);
+		audioObject.seekTo(milliseconds);
+	},
+	getCurrentPosition: function(filepath) {
+		var audioObject = audio.getObject(filepath);
+		// get media position
+	    audioObject.getCurrentPosition(
+	        // success callback
+	        function (position) {
+	            if (position > -1) {
+	                console.log((position) + " sec");
+	                audio.currPosition = position;
+	            }
+	        },
+	        // error callback
+	        function (e) {
+	            console.log("Error getting pos=" + e);
+	        }
+	    );
+	    return audio.currPosition;
+	}
+};
+
+
+
+
+
--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,84 @@
-document.addEventListener('deviceready', function() {
-    /* Javascript here... */
-    console.log('\n-------------\nDEVICE READY');
 
-    //example function
-    function changeSomeText() {
-        document.getElementById('change').innerHTML = "this text was changed by javascript";
-    }
+ons.ready(function() {
+  	/* Javascript for device api here... */
+  	console.log('\n-------------\nDEVICE READY');
 
-    //run the example function
-    changeSomeText();
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyDOoUezKPivRQbKI_dBpQC7X4aUB0vit_I",
+		authDomain: "audio-retrieve-test.firebaseapp.com",
+		databaseURL: "https://audio-retrieve-test.firebaseio.com",
+		projectId: "audio-retrieve-test",
+		storageBucket: "audio-retrieve-test.appspot.com",
+		messagingSenderId: "899690313623"
+	};
+	firebase.initializeApp(config);
+	console.log(firebase);
+
+	var database = firebase.database();
+	var ref = database.ref('stories');
+	ref.on('value', gotData, errData);
+
+
+	function gotData(data) {
+	  //console.log(data.val());
+	  var stories = data.val();
+	  var keys = Object.keys(stories);
+	  console.log(keys);
+	  for (var i = 0; i < keys.length; i++) {
+	    var k = keys[i];
+	    var title = stories[k].title;
+	    var name = stories[k].name;
+	    var longitude = stories[k].longitude;
+	    var latitude = stories[k].latitude;
+	    console.log(title, name, longitude, latitude);
+	    console.log(JSON.stringify(stories[k]));
+	    createPara(name, title, latitude, longitude);
+	    createAudio(stories[k].audioFile);
+	    createPlayBtn(stories[k].audioFile, name);
+	  }
+	}
+
+	function errData(err) {
+	  console.log("Error!");
+	  console.log(err);
+	}
+
+	//create and output paragraph tag with all the data inside it
+	//this runs for every iteration of the loop
+	function createPara(name, title, lat, lng) {
+	  var p = document.createElement('p');
+	  p.id = name;
+	  p.innerHTML = name + "<br>" + title + ", LatLng:" + lat  + ", " +  lng;
+	  document.getElementById('output').appendChild(p);  //attach it to the div id=output
+	}
+
+	//create and output audio tag 
+	//this runs for every iteration of the loop
+	function createAudio(filepath){
+	  var audio = document.createElement('audio');
+	  var source = document.createElement('source');
+	  source.src = filepath;
+	  source.type = "audio/mp4";
+	  audio.controls = "true";
+	  audio.appendChild(source);
+	  document.getElementById('output').appendChild(audio); //attach it to the div id=output         
+	}
+
+	//create a play audio button - for testing
+	//uses the cordova-plugin-media and audio-player.js
+	function createPlayBtn(filepath, name){
+	  var button = document.createElement('button');
+	  button.textContent = "play " + name + "'s audio";
+	  button.setAttribute('class', 'button');
+	  button.id = name + "-audio";
+	  document.getElementById('output').appendChild(button);  //attach it to the div id=output
+	  document.getElementById(name + "-audio").addEventListener('click', function(){
+	  	ons.notification.alert('This will play ' + name + "'s audio");
+	  	audio.play(filepath); //uses the cordova-plugin-media and audio-player.js
+	  })
+	}
+
+
 });
+
